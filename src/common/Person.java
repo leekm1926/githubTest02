182ffa3233fa8783733059f9f8534a60227ec555
leekm1926 <leekm0926@naver.com>
2020-11-06 18:01:28 +0900

    Conflict(충돌)예상됨

--- src/common/Person.java
+++ src/common/Person.java
@@ -3,9 +3,13 @@ package common;
 public class Person {
 	String name;
 	int age;
+	String nickName;
+	
 	public Person(String name, int age) {
 		super();
 		this.name = name;
 		this.age = age;
 	}
+	
+	
 }
